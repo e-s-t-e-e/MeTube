d316912aa4367b7faf1c2b3850c9c38719d7032e
e-s-t-e-e <e-s-t-e-e@users.noreply.github.com>
2026-08-02 17:05:09 +0530

    Add missing Activity and ContextWrapper imports to YoutubeWebview.java

--- app/src/main/java/com/hhst/youtubelite/browser/YoutubeWebview.java
+++ app/src/main/java/com/hhst/youtubelite/browser/YoutubeWebview.java
@@ -1,8 +1,10 @@
 package com.hhst.youtubelite.browser;
 
 import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.content.ActivityNotFoundException;
 import android.content.Context;
+import android.content.ContextWrapper;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
