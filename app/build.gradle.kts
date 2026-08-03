17dbb35258a6806291a269d1fbde7b5afd227bc4
Samarth Taksali <Samarth.Taksali@ril.com>
2026-08-03 19:22:34 +0530

    Fix version check to v1.4 and replace top logo to text-only MeTube

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,8 +3,8 @@ plugins {
     alias(libs.plugins.hilt)
 }
 
-val appVersionName = (project.findProperty("APP_VERSION_NAME") as? String) ?: "1.3"
-val appVersionCode = (project.findProperty("APP_VERSION_CODE") as? String)?.toIntOrNull() ?: 130
+val appVersionName = (project.findProperty("APP_VERSION_NAME") as? String) ?: "1.4"
+val appVersionCode = (project.findProperty("APP_VERSION_CODE") as? String)?.toIntOrNull() ?: 140
 
 android {
     namespace = "com.hhst.youtubelite"
